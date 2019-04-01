acc06a4ed5c140ca9dd0af5d496011af95b892b9
papakim <for233@naver.com>
2019-04-01 21:09:29 +0900

    수정2

--- GitTestProject/src/com/my/MainHello.java
+++ GitTestProject/src/com/my/MainHello.java
@@ -4,7 +4,7 @@ public class MainHello {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		System.out.println("Hellow World 123");
+		System.out.println("Hellow World 123 456");
 	}
 
 }
