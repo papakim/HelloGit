f8e84cf99cdd78e9d88732572a347b13fb66e9e9
papakim <for233@naver.com>
2019-04-02 04:52:52 +0900

    확인

---
+++ GitTestProject/src/com/my/MainHello2.java
@@ -0,0 +1,11 @@
+package com.my;
+
+public class MainHello2 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("Hellow World 123 456 123 789 10 11 12");
+		System.out.println("123");
+	}
+
+}
